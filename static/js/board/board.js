f7150e384787275c1f604cad9a96de7f28b88fd1
marinred <endrms1995@gmail.com>
2022-12-13 17:32:06 +0900

    Feat: Board_html,css,js  #5
    - 게시판 html,css,js 완료

--- static/js/board/board.js
+++ static/js/board/board.js
@@ -1,16 +1,87 @@
-const backend_base_url = "http://127.0.0.1:8000/"
+const backend_base_url = "http://127.0.0.1:8000"
+const frontend_base_url = "http://127.0.0.1:5500"
 
-window.onload = async function loadBoards() {
-  boards = await getBoards()
-  console.log(boards)
+
+window.onload = () => {
+  getBoard()
 }
 
-async function getBoards() {
-  const response = await fetch('http://127.0.0.1:8000/all?search=', {
-    headers : {
-      "Authorization" : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMDE2MzAzLCJpYXQiOjE2NzA2NTYzMDMsImp0aSI6ImJjYzkzNzg0YWJmYzQ0MDI5MTM3MDY5NGNjMGNkYWJhIiwidXNlcl9pZCI6MiwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.Rm8bJR-HKeGGi9MVJC4WsHS7oy8bS8wZsamPFwmLNCM"
-    },
-    method: 'GET',
-  })
-  console.log(response)
+async function getBoard() {
+  const urlParameter = window.location.search;
+  console.log(urlParameter)
+
+  if(urlParameter == ""){
+    const response = await fetch(`${backend_base_url}/board/all/`,{
+      method: 'GET',
+      headers:{
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0"
+      }
+    })
+    response_json = await response.json()
+    board_list = response_json["results"]
+    const list_html = document.getElementById("boardgetlist")
+    var count = 1
+    board_list.forEach(element => {
+      const today = new Date(element.created_at);
+      const list = `
+      <tr>
+          <td id="board_number">${count}</td>
+          <th id="board_title"><a href="http://127.0.0.1:5500/templates/board/boarddetail.html?id=${element.id}">${element.title}</a></th>
+          <td id="board_created_at">${today.toLocaleDateString()}</td>
+      </tr>`
+      count += 1
+      list_html.insertAdjacentHTML("afterbegin",list)
+    })
+  }
+  else {
+    const response = await fetch(`${backend_base_url}/board/all${urlParameter}` ,{
+      method: 'GET',
+      headers:{
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0"
+      }
+    })
+    response_json = await response.json()
+    board_list = response_json["results"]
+    const list_html = document.getElementById("boardgetlist")
+    var count = 1
+    board_list.forEach(element => {
+      const today = new Date(element.created_at);
+      const list = `
+      <tr>
+          <td id="board_number">${count}</td>
+          <th id="board_title"><a href="http://127.0.0.1:5500/templates/board/boarddetail.html?id=${element.id}">${element.title}</a></th>
+          <td id="board_created_at">${today.toLocaleDateString()}</td>
+      </tr>`
+      count += 1
+      list_html.insertAdjacentHTML("afterbegin",list)
+      }) 
+  }
 }
+
+function boardSearch(){
+  var board_search = document.getElementById("board_search").value;
+  console.log(board_search)
+  location.href = `${frontend_base_url}/templates/board/board.html?search=${board_search}`
+}
+
+
+function postBoard() {
+  location.href = "boardwrite.html"
+}
+
+
+$(document).ready(function () {
+  $("a#pageLink").click(function () {
+    $("a#pageLink").removeClass("active");
+    $(this).addClass("active");
+  });
+  $(".menu-button").click(function () {
+    $(".left-area").removeClass("hide-on-mobile");
+  });
+  $(".close-menu").click(function () {
+    $(".left-area").addClass("hide-on-mobile");
+  });
+  $(".more-button").click(function () {
+    $(".more-menu-list").toggle("hide");
+  });
+})
---
+++ static/js/board/boarddetail.js
@@ -0,0 +1,233 @@
+const backend_base_url = "http://127.0.0.1:8000"
+const urlStr = window.location.href;
+const url = new URL(urlStr);
+const urlParms = url.searchParams;
+const id = urlParms.get('id')
+
+window.onload = () => {
+  getBoardDetail()
+  webtooncomment_read()
+  
+}
+
+async function getBoardDetail() {
+  const response = await fetch(`${backend_base_url}/board/${id}/`, {
+    method: 'GET',
+  })
+  response_json = await response.json()
+  console.log(response_json)
+  const board_title = document.getElementById('board_title')
+  board_title.innerText = response_json.title
+
+  const board_user_image = document.getElementById('board_user_image')
+  board_user_image.src=`${backend_base_url}${response_json.board_user_profile_image}`
+  
+  if(response_json.image != null){
+  const board_content_image = document.getElementById('board_content_image')
+  board_content_image.innerHTML=`<img class="board-image" id="board_content_image" src="${backend_base_url}${response_json.image}">`
+  }
+  else{
+    const board_content_image = document.getElementById('board_content_image').style.display="none";
+    
+  }
+
+  const board_username = document.getElementById('board_username')
+  board_username.innerText = response_json.board_user
+
+  const board_content = document.getElementById('board_content')
+  board_content.innerHTML = response_json.content
+
+  const board_created_at = document.getElementById('board_created_at')
+  const today = new Date(response_json.created_at)
+  board_created_at.innerText = today.toLocaleDateString()
+
+  const footnote = document.getElementById('board_title')
+  console.log(response_json.board_user)
+  if(response_json.board_user=="qwe"){
+    del_put_button = `<button style="float: right; font-size: 16px;" onclick="deleteBoard()">삭제</button>
+    <button style="float: right; font-size: 16px;" onclick="putBoard()">수정</button>`
+    footnote.insertAdjacentHTML("beforeend", del_put_button)
+  }
+}
+
+async function deleteBoard() {
+  const response = await fetch(`${backend_base_url}/board/${id}/`, {
+    method: 'DELETE',
+    headers:{
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0"
+    }
+  })
+  location.href="board.html"
+}
+
+async function putBoard() {
+  location.href = `http://127.0.0.1:5500/templates/board/boardwrite.html?id=${id}`
+}
+
+
+
+
+
+
+function time2str(date) {
+  let today = new Date()
+  let before = new Date(date)
+  let time = (today - before) / 1000 / 60  // 분
+  if (time < 60) {
+      return parseInt(time) + "분 전"
+  }
+  time = time / 60  // 시간
+  if (time < 24) {
+      return parseInt(time) + "시간 전"
+  }
+  time = time / 24
+  if (time < 7) {
+      return parseInt(time) + "일 전"
+  }
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+};
+
+
+//webtoon 상세 페이지 comment 부분
+async function webtooncomment_read() {
+  const urlStr = window.location.href;
+  const url = new URL(urlStr);
+  const urlParms = url.searchParams;
+  const id = urlParms.get('id')
+  const response = await fetch(`${backend_base_url}/board/${id}/comment/`, {
+      method: 'GET'
+  })
+  response_json = await response.json()
+  console.log(response_json)
+  const comment_container = document.getElementById('comment-contatiner')
+  for (let i=0; i < response_json.length; i++){
+      let comment_id = response_json[i]['id']
+      let username = response_json[i]['username']
+      let image = response_json[i]['user_profile_image']
+      let content = response_json[i]['comment']
+      let created_at = response_json[i]['updated_at']
+      let time_before = time2str(created_at)
+      if (username == "qwe"){
+        let comment_html1 = `<div class="comment">
+                                <div class="comment_user">
+                                    <img class="user_image" style="width: 50px; height: 50px; border-radius: 70%; margin-top: 20px;" src="${backend_base_url}${image}">
+                                </div>
+                                <div class="comment_content">
+                                    <div style="display: flex;">
+                                        <h6>${username}</h6>
+                                        <span class="comment_time">${time_before}</span>
+                                    </div>
+                                    <span class="shape ble_left no_drag" id="comment">${content}</span>
+                                </div>
+                                <div class="update_delete">
+                                <span onclick="openModal(${comment_id})">수정</span>
+                                <span onclick="handleDeleteComment(${comment_id})">삭제</span>
+                                </div>
+                            </div>`
+                        
+    comment_container.insertAdjacentHTML("beforeend",comment_html1)
+    } else {
+        let comment_html2 = `<div class="comment">
+                                <div class="comment_user">
+                                    <img class="user_image" style="width: 50px; height: 50px; border-radius: 70%; margin-top: 20px;" src="${backend_base_url}${image}">
+                                </div>
+                                <div class="comment_content">
+                                    <div style="display: flex;">
+                                        <h6>${username}</h6>
+                                        <span class="comment_time">${time_before}</span>
+                                    </div>
+                                    <span class="shape ble_left no_drag" id="comment">${content}</span>
+                                </div>
+                            </div>`
+
+    comment_container.insertAdjacentHTML("beforeend",comment_html2)
+    }
+}}
+//modal 열기
+function openModal(comment_id){
+  modal.style.display = "flex"
+  webtooncomment_update_read(comment_id)
+}
+
+//modal 닫기
+function closeModal(){
+  modal.style.display = "none"
+}
+
+async function webtooncomment_write(){
+  const urlStr = window.location.href
+  console.log(urlStr)
+  const url = new URL(urlStr)
+  const urlParams = url.searchParams
+  const id = urlParams.get('id')
+  const comment_write = document.getElementById('comment_write').value
+
+  await fetch(`${backend_base_url}/board/${id}/comment/` , {
+      method:'POST',
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0",
+        "content-type" : 'application/json',
+      },
+      body: JSON.stringify({
+          "comment" : comment_write,
+      })
+  })
+  location.reload()
+}
+
+async function webtooncomment_update_read(comment_id) {
+  console.log(comment_id)
+  const urlStr = window.location.href;
+  const url = new URL(urlStr);
+  const urlParms = url.searchParams;
+  const id = urlParms.get('id')
+
+  const response = await fetch(`${backend_base_url}/board/${id}/comment/${comment_id}`, {
+      method: 'GET'
+  })
+  response_json = await response.json()
+  console.log(response_json)
+  const modal_comment_id = document.getElementById('modal_comment_id')
+  modal_comment_id.innerText = response_json.id
+  const modal_comment = document.getElementById('comment_update')
+  modal_comment.value = response_json.comment
+  }
+
+  async function webtooncomment_update() {
+    const urlStr = window.location.href
+    const url = new URL(urlStr)
+    const urlParams = url.searchParams
+    const id = urlParams.get('id')
+    const comment_id  = document.getElementById('modal_comment_id').innerText
+    const comment_update = document.getElementById('comment_update').value
+
+    await fetch(`${backend_base_url}/board/${id}/comment/${comment_id}/`, {
+        method: 'PUT',
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0",
+            "content-type" : 'application/json',
+        },
+        body: JSON.stringify({
+            "comment": comment_update,
+        })
+    })
+    closeModal()
+    location.reload()
+} 
+
+//댓글 삭제
+async function handleDeleteComment(comment_id){
+  const urlStr = window.location.href;
+  const url = new URL(urlStr);
+  const urlParms = url.searchParams;
+  const id = urlParms.get('id')
+  const response = await fetch(`${backend_base_url}/board/${id}/comment/${comment_id}/`,{
+      headers : {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMTg0MzQwLCJpYXQiOjE2NzA4MjQzNDAsImp0aSI6IjlmNzc3ZTg4YjI0ZjRhNzFhYmIwZTM3YTFkOTE4MDc2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJxd2VAcXdlLmNvbSJ9.VjL6PPlDcAzR8GizCJie61UzTRR4LLvekuZiktC8iS0",
+      },    
+      method : 'DELETE',
+      body : {}
+  })
+  window.console.log('delete')
+  location.reload()
+}
